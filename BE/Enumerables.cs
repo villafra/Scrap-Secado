03ff3850e16b1692260d39583536d3d9fc3314a7
Franco Villafañe <FrancoMiguel.Villafane@alumnos.uai.edu.ar>
2022-08-07 16:44:51 -0300

    Agregar archivos de proyecto.

---
+++ BE/Enumerables.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Enumerables
+    {
+        public enum Extrusora
+        {
+            [EnumMember(Value = "Extrusora 1")]
+            Extrusora_1 =1,
+            [EnumMember(Value = "Extrusora 2")]
+            Extrusora_2 =2,
+            [EnumMember(Value = "Extrusora 3")]
+            Extrusora_3 =3,
+        }
+        public enum Turno
+        {
+            Azul, 
+            Rojo,
+            Verde
+        }
+        public enum Familia
+        {
+            Pedigree,
+            Whiskas,
+            Eukanuba,
+            Iams,
+            Optimum,
+            Champ
+        }
+        public enum Periodo
+        {
+            P1=1,
+            P2=2,
+            P3=3,
+            P4=4,
+            P5=5,
+            P6=6,
+            P7=7,
+            P8=8,
+            P9=9,
+            P10=10,
+            P11=11,
+            P12=12,
+            P13=13
+        }
+        public enum Semana
+        {
+            W1=1,
+            W2=2,
+            W3=3,
+            W4=4
+        }
+    }
+}
---
+++ BE/Kibble Finished.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Kibble_Finished
+    {
+        public int Codigo { get; set; }
+        public string Descripción { get; set; }
+        public Enumerables.Familia Familia { get; set; }
+        public Enumerables.Extrusora Extrusora { get; set; }
+        public int Cantidad { get; set; }
+        public Enumerables.Turno Turno { get; set; }
+        public string Operador { get; set; }
+        public string Motivo { get; set; }
+        public DateTime Fecha { get; set; }
+        public Enumerables.Periodo Periodo { get; set; }
+        public Enumerables.Semana Semana { get; set; }
+
+
+        public string PeriodoPxWx()
+        {
+            return Periodo.ToString() + Semana.ToString();
+        }
+    }
+}
